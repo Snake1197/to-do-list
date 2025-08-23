bc6bd8fc5cde2e7b3d36b8dd44d62e4e6b50474b
Leandro Guerrero <112229303+Snake1197@users.noreply.github.com>
2025-08-23 15:53:30 -0500

    Actualizar TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -10,7 +10,7 @@ export default function TaskList({ tasks, onToggle, onEdit, onDelete }) {
   }
 
   return (
-    <ul className="dark:bg-white grid gap-3 wd-full">
+    <ul className="grid gap-3 wd-full">
       {tasks.map((t) => (
         <TaskItem
           key={t.id}
