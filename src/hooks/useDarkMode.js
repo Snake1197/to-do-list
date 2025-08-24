e6f435c09030afa3652744cf53c1f0a6640a51f7
Leandro Guerrero <112229303+Snake1197@users.noreply.github.com>
2025-08-24 09:57:32 -0500

    order

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+export function useDarkMode() {
+  const [darkMode, setDarkMode] = useLocalStorage("theme", false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  return [darkMode, setDarkMode];
+}
